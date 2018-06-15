fe85faf7421cd9e3cef5dd889600f65ab68c16c7
Iasmin <iasmin.ogiboski>
2018-06-14 22:45:56 -0300

    Adicionada página home(base)

---
+++ SistemaAgendamento/wwwroot/Scripts/Site/Home/Index.js
@@ -0,0 +1,11 @@
+﻿$(document).ready(function () {
+    $.get("/Agendamento").done(function (ret) {
+        $('.ConteudoSite').html(ret);
+    });
+});
+
+function Navegar(pagina) {
+    $.get("/" + pagina).done(function (ret) {
+        $('.ConteudoSite').html(ret);
+    });
+}
